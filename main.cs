4bf94417ba9b7497687459789523276ab016176b
cruzlanysraelle <31803615-cruzlanysraelle@users.noreply.replit.com>
2024-02-03 02:44:27 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,16 @@
+using System;
+
+internal class NewBaseType
+{
+    public static void Main(string[] args)
+    {
+
+        string name;
+        name = "Lan";
+        Console.WriteLine(name);
+        name = "Ysraelle";
+        Console.WriteLine(name);
+        name = "Cruz";
+        Console.WriteLine(name);
+    }
+}
